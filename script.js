1b4afa2a875c9dde564d75e5ebfb912292a08648
aka-Enzo <pjshokrzadeh@gmail.com>
2022-12-26 12:04:07 +0330

    Add function for operating on numbers based on what user chooses

--- script.js
+++ script.js
@@ -13,10 +13,22 @@ function multiplication(a, b){
 function division(a, b){
     if (b == 0) {
         alert('You Can\'t Divide a Number By Zero!');
+        return 'infinity';
     } else{
         return a / b;
     }
 }
 
+function operate(func, a, b){
+    if (func === addition){
+        return addition(a, b);
+    } else if (func === subtraction){
+        return subtraction(a, b);
+    } else if (func === multiplication){
+        return multiplication (a, b);
+    } else if (func === division){
+        return division(a, b);
+    }
+}
 
-
+console.log(operate(subtraction, 300, 50000000));
